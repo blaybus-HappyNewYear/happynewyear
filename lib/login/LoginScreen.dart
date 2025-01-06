78a07899405a7a9a3b84f6e0605b4d1965307974
choms218 <choms218@naver.com>
2025-01-06 18:45:51 +0900

    update loginscrenn detail first

--- lib/login/LoginScreen.dart
+++ lib/login/LoginScreen.dart
@@ -77,23 +77,44 @@ class _LoginState extends State<Login> {
                 children: [
                   Align(
                     alignment: Alignment.center,
-                    child: Text(
-                      "두손꼭Do전!",
-                      style: TextStyle(
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
+                    child: Text.rich(
+                      TextSpan(
+                        text: '두손꼭', // 기본 텍스트 스타일
+                        style: TextStyle(
+                          fontFamily: 'RixInooAriDuri',
+                          fontSize: 46.6,
+                          color: Colors.black87,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Do', // "Do" 부분
+                            style: TextStyle(
+                              color: Color(0xFFF95E39), // 원하는 색으로 변경
+                              fontWeight: FontWeight.bold, // 선택적으로 굵게 설정 가능
+                            ),
+                          ),
+                          TextSpan(
+                            text: '전!', // 나머지 텍스트
+                            style: TextStyle(
+                              fontFamily: 'RixInooAriDuri',
+                              fontSize: 46.6,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                  SizedBox(height: 5.0),
+                  SizedBox(height: 1.0),
                   Align(
                     alignment: Alignment.center,
                     child: Text(
                       "두헨즈 직원들의 능률향상",
                       style: TextStyle(
-                        fontSize: 13.0,
-                        color: Colors.grey[600],
+                        fontFamily: 'Pretendard',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF565656),
                       ),
                     ),
                   ),
@@ -102,43 +123,91 @@ class _LoginState extends State<Login> {
             ),
             // 입력 필드
             Padding(
-              padding: const EdgeInsets.all(40.0),
+              padding: const EdgeInsets.only(left: 20.0, top: 48.0, right: 20.0, bottom: 40.0),
               child: Column(
                 children: <Widget>[
                   // ID 입력 필드
-                  TextFormField(
-                    controller: _idController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xFFF2F2F2),
-                      hintText: "아이디를 입력하세요.",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  SizedBox(height: 20),
-                  // Password 입력 필드
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: isHiddenPassword,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xFFF2F2F2),
-                      hintText: "비밀번호를 입력하세요.",
-                      suffixIcon: InkWell(
-                        onTap: _togglePasswordView,
-                        child: Icon(
-                          isHiddenPassword
-                              ? Icons.visibility_off
-                              : Icons.visibility,
+                  Container(
+                    height: 52.0,
+                    child: TextFormField(
+                      controller: _idController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFFFFFFF),
+                        hintText: "아이디를 입력하세요.",
+                        hintStyle: TextStyle(
+                            color: Color(0xFFC7C7C7),
+                            fontSize: 16.0
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(
+                            color: Color(0xFFC7C7C7), // 기본 테두리 색상
+                            width: 1.0, // 기본 테두리 두께
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(
+                            color: Color(0xFFC7C7C7), // 활성화된 상태에서의 테두리 색상
+                            width: 1.0, // 테두리 두께
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(
+                            color: Color(0xFF7879F1), // 포커스 상태에서의 테두리 색상 (여기서는 파란색)
+                            width: 2.0, // 포커스 상태에서 테두리 두께
+                          ),
                         ),
                       ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide.none,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  // Password 입력 필드
+                  Container(
+                    height: 52.0,
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: isHiddenPassword,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFFFFFFF),
+                        hintText: "비밀번호를 입력하세요.",
+                        hintStyle: TextStyle(
+                           color: Color(0xFFC7C7C7)
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: _togglePasswordView,
+                          child: Icon(
+                            isHiddenPassword
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                              color: isHiddenPassword ? Color(0xFFC7C7C7) : Colors.black
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(
+                            color: Color(0xFFC7C7C7), // 기본 테두리 색상
+                            width: 1.0, // 기본 테두리 두께
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(
+                            color: Color(0xFFC7C7C7), // 활성화된 상태에서의 테두리 색상
+                            width: 1.0, // 테두리 두께
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(
+                            color: Color(0xFF7879F1), // 포커스 상태에서의 테두리 색상
+                            width: 2.0, // 포커스 상태에서 테두리 두께
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -153,12 +222,14 @@ class _LoginState extends State<Login> {
                             isRememberMe = value!;
                           });
                         },
+                        activeColor: Color(0xFFF95E39), // 체크박스 선택된 상태 색상 (주황색)
+                        checkColor: Colors.white, // 체크된 상태의 체크 표시 색상 (흰색)
                       ),
                       Text(
                         "로그인 유지",
                         style: TextStyle(
                           fontSize: 14.0,
-                          color: Colors.grey[700],
+                          color: Colors.black,
                         ),
                       ),
                     ],
@@ -168,9 +239,9 @@ class _LoginState extends State<Login> {
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       minimumSize: Size(double.infinity, 50.0),
-                      backgroundColor: isButtonActive ? Colors.orangeAccent : Colors.grey,
+                      backgroundColor: isButtonActive ? Color(0xFFF95E39) : Color(0xFFF6F6F6),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16.0),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
                     onPressed: isButtonActive
@@ -183,7 +254,7 @@ class _LoginState extends State<Login> {
                       "로그인",
                       style: TextStyle(
                         fontSize: 18.0,
-                        color: Colors.black,
+                        color: isButtonActive? Colors.white : Color(0xFFC7C7C7),
                       ),
                     ),
                   ),
@@ -192,9 +263,9 @@ class _LoginState extends State<Login> {
                   TextButton(
                     style: TextButton.styleFrom(
                       minimumSize: Size(double.infinity, 50.0),
-                      side: BorderSide(color: Colors.orangeAccent),
+                      side: BorderSide.none,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16.0),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
                     onPressed: () {
@@ -204,7 +275,7 @@ class _LoginState extends State<Login> {
                       "회원가입",
                       style: TextStyle(
                         fontSize: 18.0,
-                        color: Colors.orangeAccent,
+                        color: Color(0xFFF95E39),
                       ),
                     ),
                   ),
