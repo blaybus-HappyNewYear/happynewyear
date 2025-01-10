3502ff1e00b39eeb68598a40fbf62476f395d5c1
choms218 <choms218@naver.com>
2025-01-10 19:18:45 +0900

    noticeboard appbar edit

--- lib/notice/NoticeScreen.dart
+++ lib/notice/NoticeScreen.dart
@@ -85,7 +85,27 @@ class _BoardPageState extends State<BoardPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('게시판'),
+        backgroundColor: Colors.white,
+        title:Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(top: 10.0),
+          child: Text(
+            "게시판",
+            style: TextStyle(
+              fontFamily: 'Pretendard',
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(2.0), // 보더의 높이를 설정
+          child: Container(
+            color: Color(0xFFEAEAEA), // 보더 색상
+            height: 1.0, // 보더 두께
+          ),
+        ),
       ),
       body: Column(
         children: [
